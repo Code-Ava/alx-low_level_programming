4ecdae718dc3e1150b2c097be243bbc577d585fa
Code-Ava <ebunmi15@gmail.com>
2023-02-21 15:55:17 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z' lett++) 
+		_putchar(letter);
+
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @nld: number's last digit result
+ * Return: value of the last digit
+ */
+int print_last_digit(int nld)
+{
+	int prev; 
+
+	prev = (nld % 10);
+
+	if (prev < 0)
+	{
+		prev = (-1 * prev);
+	}
+
+	_putchar(prev + '0');
+	return (prev);
+}
